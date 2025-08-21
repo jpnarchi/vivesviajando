f88247e23e1de3e7c28c4de43d07b2b41a4950c5
jpnarchi <142521385+jpnarchi@users.noreply.github.com>
2025-08-20 18:08:20 -0600

    Cambios imagenes

--- components/Features.tsx
+++ components/Features.tsx
@@ -6,7 +6,7 @@ const Features = () => {
   return (
     <section className="flex-col flexCenter overflow-hidden bg-feature-bg bg-center bg-no-repeat py-24">
       <div className="max-container padding-container relative w-full flex justify-end">
-        {/* <div className="flex flex-1 lg:min-h-[900px]">
+        <div className="flex flex-1 lg:min-h-[900px]">
           <Image
             src="/phone.png"
             alt="phone"
@@ -14,7 +14,7 @@ const Features = () => {
             height={1000}
             className="feature-phone"
           />
-        </div> */}
+        </div>
 
         <div className="z-20 flex w-full flex-col lg:w-[60%]">
           <div className='relative'>
