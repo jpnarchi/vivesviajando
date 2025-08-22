be97195283e3ef88fdf598c99d7e0f2643d0ad68
jpnarchi <142521385+jpnarchi@users.noreply.github.com>
2025-08-22 10:54:04 -0600

    Cambio de logo

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -20,7 +20,7 @@ const Navbar = () => {
   return (
     <nav className="flexBetween max-container padding-container relative z-30 py-5">
       <Link href="/">
-        <Image src="/logo.png" alt="logo" width={150} height={40} />
+        <Image src="/logo.png" alt="logo" width={120} height={40} />
       </Link>
 
       <ul className="hidden h-full gap-12 lg:flex">
